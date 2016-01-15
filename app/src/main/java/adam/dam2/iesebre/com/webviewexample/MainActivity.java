ab4642fc0964112ff833be274c00f523cb7748d0
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-01-15 15:58:17 +0100

    Creating first example WebView Android

---
+++ app/src/main/java/adam/dam2/iesebre/com/webviewexample/MainActivity.java
@@ -0,0 +1,17 @@
+package adam.dam2.iesebre.com.webviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) this.findViewById(R.id.webView);
+        myWebView.loadUrl("acacha.org");
+    }
+}
